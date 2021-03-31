e3054f63d34e544fc35a14832414659c9e5e0c66
Jacek Centkowski <geminica.programs@gmail.com>
2021-03-31 13:28:42 +0200

    Model JPA Enumation
    Add Difficulty enum to the Recipe entity

---
+++ src/main/java/geminica/guru/springframework/recipe/domain/Difficulty.java
@@ -0,0 +1,7 @@
+package geminica.guru.springframework.recipe.domain;
+
+public enum Difficulty {
+  EASY,
+  MODERATE,
+  HARD
+}
--- src/main/java/geminica/guru/springframework/recipe/domain/Recipe.java
+++ src/main/java/geminica/guru/springframework/recipe/domain/Recipe.java
@@ -3,6 +3,8 @@ package geminica.guru.springframework.recipe.domain;
 import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -28,6 +30,9 @@ public class Recipe {
   private Set<Ingredient> ingredients;
 
   @Lob private Byte[] image;
+
+  @Enumerated(value = EnumType.STRING)
+  private Difficulty difficulty;
 
   @OneToOne(cascade = CascadeType.ALL)
   private Notes notes;
@@ -110,6 +115,14 @@ public class Recipe {
 
   public void setImage(Byte[] image) {
     this.image = image;
+  }
+
+  public Difficulty getDifficulty() {
+    return difficulty;
+  }
+
+  public void setDifficulty(Difficulty difficulty) {
+    this.difficulty = difficulty;
   }
 
   public Notes getNotes() {
