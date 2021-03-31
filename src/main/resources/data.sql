b070d2076e7ad4c0b5dfb68a5eed630ac04c2600
Jacek Centkowski <geminica.programs@gmail.com>
2021-03-31 16:28:07 +0200

    Database Initialization with Spring

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO category (name) VALUES ('American');
+INSERT INTO category (name) VALUES ('Polish');
+INSERT INTO category (name) VALUES ('Italian');
+INSERT INTO category (name) VALUES ('Mexican');
+INSERT INTO category (name) VALUES ('Fast Food');
+
+INSERT INTO unit_of_measure (uom) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure (uom) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure (uom) VALUES ('Glass');
+INSERT INTO unit_of_measure (uom) VALUES ('Pinch');
+INSERT INTO unit_of_measure (uom) VALUES ('DKG');
