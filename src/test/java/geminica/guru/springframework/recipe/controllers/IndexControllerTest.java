5fc205653d22a7b0b6e4c5ab9db4291e05afcbb8
Jacek Centkowski <geminica.programs@gmail.com>
2021-04-17 18:58:17 +0200

    Add argument capture to unit controller test

--- src/test/java/geminica/guru/springframework/recipe/controllers/IndexControllerTest.java
+++ src/test/java/geminica/guru/springframework/recipe/controllers/IndexControllerTest.java
@@ -1,14 +1,17 @@
 package geminica.guru.springframework.recipe.controllers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import geminica.guru.springframework.recipe.domain.Recipe;
 import geminica.guru.springframework.recipe.services.RecipeService;
+import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.ui.Model;
@@ -27,12 +30,22 @@ class IndexControllerTest {
 
   @Test
   void testIndexPage() {
+    // given
+    Recipe r1 = new Recipe();
+    r1.setId(1l);
+    Recipe r2 = new Recipe();
+    r2.setId(2l);
+    when(recipeService.findAll()).thenReturn(Set.of(r1, r2));
+    ArgumentCaptor<Set<Recipe>> captor = ArgumentCaptor.forClass(Set.class);
+
     // when
     String result = indexController.getIndexPage(model);
 
     // then
     assertEquals("index", result);
     verify(recipeService).findAll();
-    verify(model).addAttribute(eq("recipes"), anySet());
+    verify(model).addAttribute(eq("recipes"), captor.capture());
+    Set<Recipe> setInController = captor.getValue();
+    assertEquals(2, setInController.size());
   }
 }
