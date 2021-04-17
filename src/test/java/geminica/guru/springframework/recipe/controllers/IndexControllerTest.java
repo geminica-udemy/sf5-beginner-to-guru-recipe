53b0d7ff8ca08bee8192abaee7f5ede406772146
Jacek Centkowski <geminica.programs@gmail.com>
2021-04-17 19:04:24 +0200

    Add simple MockMvc to controller unit test

--- src/test/java/geminica/guru/springframework/recipe/controllers/IndexControllerTest.java
+++ src/test/java/geminica/guru/springframework/recipe/controllers/IndexControllerTest.java
@@ -4,6 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import geminica.guru.springframework.recipe.domain.Recipe;
 import geminica.guru.springframework.recipe.services.RecipeService;
@@ -14,6 +17,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,6 +31,12 @@ class IndexControllerTest {
   @BeforeEach
   void setUp() {
     indexController = new IndexController(recipeService);
+  }
+
+  @Test
+  void testMockMvc() throws Exception {
+    MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+    mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("index"));
   }
 
   @Test
