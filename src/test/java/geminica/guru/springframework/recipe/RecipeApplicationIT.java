16085cdf0ddd3aee52a92208bb4910211d8b71fe
Jacek Centkowski <geminica.programs@gmail.com>
2021-04-17 19:34:25 +0200

    Use failsafe plugin to run integration tests in 'verify' phase

--- src/test/java/geminica/guru/springframework/recipe/RecipeApplicationIT.java
+++ src/test/java/geminica/guru/springframework/recipe/RecipeApplicationIT.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RecipeApplicationTests {
+class RecipeApplicationIT {
 
   @Test
   void contextLoads() {}
